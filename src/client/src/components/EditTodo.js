f3175a7795248ab93d1714a3f73201ee4b6f2811
gayankanishka <gkanishka6@gmail.com>
2020-03-22 20:47:31 +0530

    document formatting

--- src/client/src/components/EditTodo.js
+++ src/client/src/components/EditTodo.js
@@ -1,77 +1,95 @@
-import React, { Fragment, useState } from 'react';
+import React, { Fragment, useState } from "react";
 
 const EditTodo = ({ todo }) => {
+  const [description, setDescription] = useState(todo.description);
 
-    const [description, setDescription] = useState(todo.description);
+  async function updateDescription(e) {
+    try {
+      e.preventDefault();
 
-    async function updateDescription(e) {
-        try {
-            e.preventDefault();
-
-            const body = { description };
-            const response = await fetch(`http://localhost:5000/todos/${todo.todo_id}`, {
-                method: 'PUT',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify(body)
-            });
-
-            // TODO: need to find a better way to this
-            window.location = '/';
-        } catch (error) {
-            console.error(error.message);
+      const body = { description };
+      const response = await fetch(
+        `http://localhost:5000/todos/${todo.todo_id}`,
+        {
+          method: "PUT",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(body)
         }
+      );
+
+      // TODO: need to find a better way to this
+      window.location = "/";
+    } catch (error) {
+      console.error(error.message);
     }
+  }
 
-    return (
-        <Fragment>
-            <button type="button" className="btn btn-warning" data-toggle="modal" data-target={`#id${todo.todo_id}`}>
-                Edit
-            </button>
+  return (
+    <Fragment>
+      <button
+        type="button"
+        className="btn btn-warning"
+        data-toggle="modal"
+        data-target={`#id${todo.todo_id}`}
+      >
+        Edit
+      </button>
 
-            <div className="modal fade" id={`id${todo.todo_id}`} tabIndex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" onClick={() => setDescription(todo.description)}>
-                <div className="modal-dialog">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h5 className="modal-title">Edit Todo</h5>
-                            <button
-                                type="button"
-                                className="close"
-                                data-dismiss="modal"
-                                aria-label="Close"
-                                onClick={() => setDescription(todo.description)}>
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-
-                        <div className="modal-body">
-                            <input
-                                type="text"
-                                className="form-control"
-                                value={description}
-                                onChange={e => setDescription(e.target.value)}
-                            />
-                        </div>
-                        <div className="modal-footer">
-                            <button
-                                type="button"
-                                className="btn btn-warning"
-                                data-dismiss="modal"
-                                onClick={e => updateDescription(e)}>
-                                Edit
-                            </button>
-                            <button
-                                type="button"
-                                className="btn btn-secondary"
-                                data-dismiss="modal"
-                                onClick={() => setDescription(todo.description)}>
-                                Close
-                            </button>
-                        </div>
-                    </div>
-                </div>
+      <div
+        className="modal fade"
+        id={`id${todo.todo_id}`}
+        tabIndex="-1"
+        role="dialog"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+        onClick={() => setDescription(todo.description)}
+      >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title">Edit Todo</h5>
+              <button
+                type="button"
+                className="close"
+                data-dismiss="modal"
+                aria-label="Close"
+                onClick={() => setDescription(todo.description)}
+              >
+                <span aria-hidden="true">&times;</span>
+              </button>
             </div>
-        </Fragment>
-    );
-}
+
+            <div className="modal-body">
+              <input
+                type="text"
+                className="form-control"
+                value={description}
+                onChange={e => setDescription(e.target.value)}
+              />
+            </div>
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-warning"
+                data-dismiss="modal"
+                onClick={e => updateDescription(e)}
+              >
+                Edit
+              </button>
+              <button
+                type="button"
+                className="btn btn-secondary"
+                data-dismiss="modal"
+                onClick={() => setDescription(todo.description)}
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
 
 export default EditTodo;
--- src/server/db.js
+++ src/server/db.js
@@ -1,11 +1,11 @@
-const Pool = require('pg').Pool;
+const Pool = require("pg").Pool;
 
 const pool = new Pool({
-    user: 'postgres',
-    password: '1qaz2wsx',
-    host: 'localhost',
-    port: 5432,
-    database: 'perntodo'
+  user: "postgres",
+  password: "1qaz2wsx",
+  host: "localhost",
+  port: 5432,
+  database: "perntodo"
 });
 
 module.exports = pool;
